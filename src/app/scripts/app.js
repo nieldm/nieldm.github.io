6314c160cbbae745be0190c030abf71bfdba900f
kurama <daniel.mendez@onrecords.com>
2014-10-28 10:52:48 -0430

    First Commit

---
+++ src/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nielResumeApp
+ * @description
+ * # nielResumeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nielResumeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ src/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nielResumeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nielResumeApp
+ */
+angular.module('nielResumeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
